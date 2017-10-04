1ae09e53f414abbdd3c542c1bd8f757c700bdd3d
Gabriel Cintra <gabrielcinntra@gmail.com>
2017-10-04 16:10:58 -0300

    Art Update

---
+++ Set/Assets/Scripts/Behaviours/RandomMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMove : MonoBehaviour 
+{
+	public float verticalSpeed, amplitude;
+	private Vector3 _startPosition;
+ 
+	public void Start () 
+	{
+	    _startPosition = transform.position;
+	}
+	 
+	public void Update()
+	{
+	     transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude + 2.35f, 0.0f);
+	}
+}
